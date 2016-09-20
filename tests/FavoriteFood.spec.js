45dc86f3e9ad52ec5d1b81d1591e293d1fc28240
Bradley Reiss <bradleyreiss@Bradleys-MacBook-Air.local>
2016-09-20 13:24:06 -0400

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -1,11 +1,16 @@
 describe('Favorite Food Filter', function () {
-	var $controller;
+	var $controller, favoriteFood;
 
 	beforeEach(module('app'));
 
-	beforeEach(inject(function ($injector) {
-		$filter = $injector.get('$filter');
-	}));
+	beforeEach(inject(function ($filter) {
+        favoriteFood = $filter('favoriteFood');
+    }));
+
+	it('should remove all vowels', function () {
+		var data = [{name: 'brad', favoriteFood: 'broccoli'}];
+        expect(favoriteFood(data, 'broccoli')).toEqual([{ name: 'brad', favoriteFood: 'broccoli' }]);
+    });
 
 	
 });
